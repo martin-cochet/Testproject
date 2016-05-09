7a01ae91177b00a2ad3f1c86cb4494f088e11730
martin-cochet <martincochet@yahoo.fr>
2016-05-09 21:15:47 +0200

    Utilisation d'une liste
    Au lieu de devoir faire plein de if... pour le numero de la case, on
    peut utiliser une liste.
    Il y a un petit tutoriel ici:
    
    http://apprendre-python.com/page-apprendre-listes-list-tableaux-tableaux-liste-array-python-cours-debutant
    
    Deux pieges:
    -Le premier element d'une liste a le numero 0 et pas 1. C'est pour ca
    que j'ai du creer une liste cases a 10 elements et appeler le 1er '' (on
    ne va pas s'en servir ici)
    -raw_input retourne une chaine de caracteres et pas un entier.  Pour que
    question1 soit compris comme d'index de la liste il faut convertir en
    entier avec la fonction int. (Integer en anglais).

--- morpion.py
+++ morpion.py
@@ -10,64 +10,31 @@ case6 = ' '
 case7 = ' '
 case8 = ' '
 case9 = ' '
-
+cases = ['',' ',' ',' ',' ',' ',' ',' ',' ',' ']
 pion = (raw_input('quel pion veut tu prendre joueur1 (O/X)? '))
 if pion == 'X':
         pion2 = 'O'
         print 'joueur 2 prend les O'
 else:
+		pion2 = 'X' #Il faut que pion2 soit defini dans tous les cas
 		print 'joueur 2 prend les X'
 
 
-def grille (case1, case2, case3, case4, case5, case6, case7, case8, case9):
-  print '|' + case1 + '|' + case2 + '|' + case3 + '|'
-  print '|' + case4 + '|' + case5 + '|' + case6 + '|'
-  print '|' + case7 + '|' + case8 + '|' + case9 + '|'
+def grille (cases):
+  print '|' + cases[1] + '|' + cases[2] + '|' + cases[3] + '|'
+  print '|' + cases[4] + '|' + cases[5] + '|' + cases[6] + '|'
+  print '|' + cases[7] + '|' + cases[8] + '|' + cases[9] + '|'
   return
 
 for n in range(1,6):
-    question1 = raw_input('ou veut tu jouer joueur1 ? ')
-    if question1 == '1':
-        case1 = pion
-    if question1 == '2':
-        case2 = pion
-    if question1 == '3':
-        case3 = pion
-    if question1 == '4':
-        case4 = pion
-    if question1 == '5':
-        case5 = pion
-    if question1 == '6':
-        case6 = pion
-    if question1 == '7':
-        case7 = pion
-    if question1 == '8':
-        case8 = pion
-    if question1 == '9':
-        case9 = pion
-    grille (case1, case2, case3, case4, case5, case6, case7, case8, case9)
+    question1 = int(raw_input('ou veut tu jouer joueur1 ? '))
+    cases[question1]=pion
+    grille (cases)
     if n == 5:
         break 
-    question2 = raw_input('ou veut tu jouer joueur 2 ? ')
-    if question2 == '1':
-        case1 = pion2
-    if question2 == '2':
-        case2 = pion2
-    if question2 == '3':
-        case3 = pion2
-    if question2 == '4':
-        case4 = pion2
-    if question2 == '5':
-        case5 = pion2
-    if question2 == '6':
-        case6 = pion2
-    if question2 == '7':
-        case7 = pion2
-    if question2 == '8':
-        case8 = pion2
-    if question2 == '9':
-        case9 = pion2
-    grille (case1, case2, case3, case4, case5, case6, case7, case8, case9)
+    question2 = int(raw_input('ou veut tu jouer joueur 2 ? '))
+    cases[question2]=pion2
+    grille (cases)
 
 print 'End'
 
